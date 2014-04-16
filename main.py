ee251ded8fdd37de5459f67361e65d595c30cb86
Mark Cahill <mark@thinkjson.com>
2014-04-15 22:59:43 -0400

    Made more progress

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify, request, abort
+from slugify import slugify
 from hashlib import sha256
+from models import *
 import os
 
 app = Flask(__name__)
@@ -8,19 +10,59 @@ app = Flask(__name__)
 def index():
     """Return a friendly HTTP greeting."""
     hash = sha256(os.urandom(1024)).hexdigest()
-    return '<a href="/{hash}">New account</a>'.format(hash=hash)
+    return render_template('index.html', hash=hash)
 
 @app.route('/<hash>')
 def account(hash):
     """View a list of counters"""
-    return 'Save this page. This is your new account, accessible to anyone with the url.'
+    return render_template('account.html')
 
+@app.route('/<hash>/counters', methods=['GET', 'POST'])
+def counter_list(hash):
+	"""List counters"""
+	counters = []
+	results = Counter.query()
+
+	# Create new counter
+	if request.method == 'POST':
+		counter = Counter(name=request.form.get('name'), id=slugify(request.form.get('name')))
+		if counter.name is None:
+			abort('name is required')
+		counter.put()
+		counters.append({
+			"id": counter.key.id(),
+			"name": counter.name,
+			"created_dt": counter.created_dt.strftime('%Y-%m-%d %H:%M:%S'),
+			"entries": []
+		})
+	
+	# Return list of counters
+	for counter in results:
+		output = {
+			"id": counter.key.id(),
+			"name": counter.name,
+			"created_dt": counter.created_dt.strftime('%Y-%m-%d %H:%M:%S'),
+			"entries": []
+		}
+		for entry in counter.entries:
+			output['entries'].append({
+				"date": entry.date,
+				"count": entry.count
+			})
+		counters.append(output)
+	return jsonify(counters=counters)
+
+@app.route('/<hash>/counters/<counter_id>', methods=['POST'])
+def counter(hash, counter_id):
+	counter = ndb.Key(Counter, counter_id).get()
+	if counter is None:
+		abort('Could not find entity')
+	return jsonify(counter)
 
 @app.errorhandler(404)
 def page_not_found(e):
     """Return a custom 404 error."""
     return '404 Not Found', 404
-
 
 @app.errorhandler(500)
 def page_not_found(e):
